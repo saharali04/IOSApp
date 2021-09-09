6beffd1e277f714596fe6f69972f32854b3ccaa7
Sahar Ali <saharnazimali@gmail.com>
2021-09-09 11:38:08 -0400

    Initial Commit

---
+++ FoodOrdering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrdering
+//
+//  Created by Sahar Ali on 9/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
