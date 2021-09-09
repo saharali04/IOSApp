7ce4ef4343af9591283cd1e453eceaa683cf0bac
Sahar Ali <saharnazimali@gmail.com>
2021-09-09 15:13:18 -0400

    Basic setup for IOS app

--- FoodOrdering/ViewController.swift
+++ FoodOrdering/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var restaurantLbl: UILabel!
+    @IBOutlet weak var tipTxt: UITextField!
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalPriceLbl.text = ""
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buyItems(_ sender: Any) {
+        let tip = Double(tipTxt.text!)!
+        let totalPrice = tip + 15.00
+        totalPriceLbl.text = "$\(totalPrice)"
+        
+    }
 }
 
